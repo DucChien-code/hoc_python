1d8510440206adcd6644820fc97111f8ac2f3680
DucChien-code <chiendoghe@gmail.com>
2023-03-20 16:18:38 +0700

    first commit

---
+++ 1.py
@@ -0,0 +1,22 @@
+import math
+
+
+def kiemtra(a, b, c):
+    if a == 0:
+        nghiem = -b/a
+    else:
+        landa = b*b - 4*a*c
+        if landa < 0:
+            print("Phương trình vô nghiệm ❤️")
+        elif landa == 0:
+            nghiem = -b/2*c
+        else:
+            nghiem1 = (-b - math.sqrt(landa))/2*a
+            nghiem2 = (-b + math.sqrt(landa))/2*a
+    return nghiem
+
+
+a = int(input("Nhap a= "))
+b = int(input("Nhap b= "))
+c = int(input("Nhap c= "))
+print(kiemtra(a, b, c))
