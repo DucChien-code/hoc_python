2cd32aca95e3ee65323fd1172777a2206bcaa128
DucChien-code <chiendoghe@gmail.com>
2023-03-24 22:05:41 +0700

    code - chơi

--- class.py
+++ class.py
@@ -4,7 +4,7 @@ class Student:
         self.age = age
         self.address = address
         self.classe = classe
-    
+
     def show_info(self):
         print(f''' STUDENT INFO👌
 Name: {self.name}❤️
@@ -24,3 +24,36 @@ Lop_hoc = input("Lop cua ban: ")
 out_student = Student(Ten, Tuoi, Que, Lop_hoc)
 
 out_student.show_info()
+
+class People:
+    def __init__(self, birthday) -> None:
+        self.birthday = birthday
+    @property # coi các hàm như là một thuộc tính
+    def get_age(self):
+        return 2023 - self.birthday
+
+people1 = People(2003)
+
+age = people1.get_age
+
+print(age)
+
+class Human:
+    def __init__(self, name):  
+        self.name = name
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Student(Human):
+    def __init__(self, name, age):
+        super().__init__(name) # super() gọi class Human tạo object
+        self.age = age
+
+    def __str__(self):
+        return f"Name: {self.name}, age: {self.age}"
+
+
+student_one = Student("Chien", 20)
+print(student_one)
---
+++ file.py
@@ -0,0 +1,19 @@
+# File
+"""
+b1: open
+b2: process
+b3: close
+"""
+# fp = open("test.txt", mode='a')  # r => reading đọc file, a => append, w => writing
+
+# data = fp.write("Le Duc Tiep\n")
+
+# fp.close()
+
+lst = ['a', 1, True, "c", 4.5]
+
+fp1 = open("test.txt", mode="w")
+
+data = fp1.write(' - '.join(map(str,lst)))
+
+fp1.close
